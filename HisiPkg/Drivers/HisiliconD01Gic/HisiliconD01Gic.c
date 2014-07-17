98ee30e86c835072c2a0aa583be4908ba62f062e
Wei Xu <xuwei5@hisilicon.com>
2014-07-17 16:50:56 +0800

    HisiPkg: use ARM GIC Lib to avoid compile error issue
    Signed-off-by: Wei Xu <xuwei5@hisilicon.com>

--- HisiPkg/Drivers/HisiliconD01Gic/HisiliconD01Gic.c
+++ HisiPkg/Drivers/HisiliconD01Gic/HisiliconD01Gic.c
@@ -39,33 +39,3 @@ ArmGicSendSgiTo (
   MmioWrite32 (GicDistributorBase + ARM_GIC_ICDSGIR, ((TargetListFilter & 0x3) << 24) | ((CPUTargetList & 0xFF) << 16) | SgiId);
 }
 
-RETURN_STATUS
-EFIAPI
-ArmGicAcknowledgeInterrupt (
-  IN  UINTN          GicDistributorBase,
-  IN  UINTN          GicInterruptInterfaceBase,
-  OUT UINTN          *CoreId,
-  OUT UINTN          *InterruptId
-  )
-{
-  UINT32            Interrupt;
-
-  // Read the Interrupt Acknowledge Register
-  Interrupt = MmioRead32 (GicInterruptInterfaceBase + ARM_GIC_ICCIAR);
-
-  // Check if it is a valid interrupt ID
-  if ((Interrupt & 0x3FF) < ArmGicGetMaxNumInterrupts (GicDistributorBase)) {
-    // Got a valid SGI number hence signal End of Interrupt by writing to ICCEOIR
-    MmioWrite32 (GicInterruptInterfaceBase + ARM_GIC_ICCEIOR, Interrupt);
-
-    if (CoreId) {
-      *CoreId = (Interrupt >> 10) & 0x7;
-    }
-    if (InterruptId) {
-      *InterruptId = Interrupt & 0x3FF;
-    }
-    return RETURN_SUCCESS;
-  } else {
-    return RETURN_INVALID_PARAMETER;
-  }
-}
